d46cf2483066d1fbd32da7bf96fa338bbf0d8605
roworo rod <roworo rod>
2016-08-02 16:36:53 -0600

    UML y clase personaje

---
+++ Programacion/Personaje.java
@@ -0,0 +1,26 @@
+/******************************
+*
+*
+*
+*
+******************************/
+
+public class Personaje{
+
+	public void walk(){
+
+	}
+
+	public void turnRight(){
+
+	}
+
+	public void turnLeft(){
+
+	}
+
+	public void Personnaje(){
+	
+	}
+	
+}
